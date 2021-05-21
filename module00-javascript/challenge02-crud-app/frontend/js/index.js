81ef3ae75b97b917bdb2920871f4d002ed335596
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2021-05-21 11:06:44 +0100

    refactor loading data, display and selected elements
    Refactor loading of data methods , display of this data (into lists and select options), and selected items

--- module00-javascript/challenge02-crud-app/frontend/js/index.js
+++ module00-javascript/challenge02-crud-app/frontend/js/index.js
@@ -1,13 +1,62 @@
-// set variables
+// set global variables
 let dataEmployees = [];
 let dataRoles = [];
+let itemSelected;
 
-function init() {
-    // set themethods to be called once the page is load
-    fetchData();
+const clearButton = document.querySelector('#clear');
+const form = document.querySelector('form');
+
+async function init() {
+    // set the methods to be called once the page is load
+    await fetchData();
+    readEmployees();
+    readRoles();
+
+    // event listeners
+    clearButton.addEventListener('click', () => clearData(event));
+}
+init();
+
+function selectItem(employee, event) {
+    clearSelection();
+    // save employee into the global variable itemSelected to hold its value
+    itemSelected = employee;
+    // add a class 'selected' to the selected item, adding a style to it
+    // for that, use a variable to hold the event.target value  
+    let element = event.target;
+    // then, add the class
+    element.classList.add('selected');
+    console.log(itemSelected);
+    // once we have the element select from the list, we should display its values on the form 
+    // lets use query selector form to access the form elements
+    form.name.value = employee.name;
+    form.salary.valueAsNumber = employee.salary;
+    form.role_id.value = employee.role_id;
 }
 
-init();
+// removing class selected
+function clearSelection() {
+    // when called, this function will get by querySelector which li has the class selected on it
+    let elementSelected = document.querySelector('li.selected');
+    // then, through a statement will check:
+    // if the found item is not null/empty
+    if (elementSelected != null) {
+        // then, remove the class selected
+        elementSelected.classList.remove('selected');
+    }
+    // once is done, call this function at the beginning of the selectItem() method
+    // the function will remove a selected class before add a new one
+    // also, apply his function to the clearData(), to run every time the clear button is clicked
+}
+
+// clear all inputs and remove selected class
+function clearData(event) {
+    event.preventDefault();
+    form.name.value = "";
+    form.salary.value = "";
+    form.role_id.value = "";
+    clearSelection();
+}
 
 // considering that we are reading two different data, we want to grab their results at once
 // the default behaviour for the requests is: once one is finished, then, start the second
@@ -21,38 +70,58 @@ async function fetchData() {
                 listEmployees(),
                 listRoles(),
             ])
-
-        // call the function to read and manage the data
-        let setEmployeesList = readEmployees();
-        // and set it into our html
-        document.querySelector('#list-employees').innerHTML = setEmployeesList;
-
-        let setRolesOptions = readRoles(dataRoles);
-        // set the result into the select form
-        document.querySelector('#role_id').innerHTML = setRolesOptions;
     } catch (error) {
         showError(error);
     }
+    console.log("fecthing data")
 }
 
 function readEmployees() {
+    // variables
+    const employeesList = document.querySelector('#list-employees');
+
     // first, read the employes data
-    let list = dataEmployees.map((employee) => {
-        // second, findo in roles the role id that matches (==) the employee role_id
+    dataEmployees.forEach((employee) => {
+        // second, find in roles the role id that matches (==) the employee role_id
         // save this result into a variable
         let listRole = dataRoles.find((role) => role.id == employee.role_id);
-        // then, return the data we want into a <li> element
-        return `<li>${employee.name}, ${listRole.name} - ${employee.salary}</li>`
+        // then, for each item, create and element
+        const li = document.createElement('li');
+        const spanName = document.createElement('span');
+        spanName.textContent = employee.name + ",";
+        const spanRole = document.createElement('span');
+        spanRole.textContent = listRole.name + ",";
+        const spanSalary = document.createElement('span');
+        spanSalary.textContent = employee.salary;
+        // append spans to li
+        li.appendChild(spanName);
+        li.appendChild(spanRole);
+        li.appendChild(spanSalary)
+        // finally, append li to ul
+        employeesList.appendChild(li);
+
+        // for each li, we will add a event listener, to identify which item was clicked
+        li.addEventListener('click', () => selectItem(employee, event));
     })
-    // using .join("") to get rid off commas in each <li>
-    return list.join("");
+    console.log("read employess data")
 }
 
 function readRoles() {
-    let options = dataRoles.map((item) => {
-        return `<option value="role_id">${item.name}</option>`
+    // read the employes data
+    dataRoles.forEach((role) => {
+        // then, for each item, create and element
+        const option = document.createElement('option');
+        // set its context and values
+        option.textContent = role.name;
+        option.value = role.id;
+        // and finally, append option to the select
+        form.role_id.appendChild(option);
     })
-    return options;
+    console.log("read roles data")
+}
+
+function showError(error) {
+    document.querySelector('#errors').innerHTML = "Loading data error";
 }
 
 

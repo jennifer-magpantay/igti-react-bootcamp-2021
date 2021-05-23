1f78ea89a2dd94c270f687c37142b9daa45a247d
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2021-05-23 15:05:30 +0100

    set crud requests
    Crud requests set in the application, updating front and back ends

--- module00-javascript/challenge02-crud-app/frontend/js/index.js
+++ module00-javascript/challenge02-crud-app/frontend/js/index.js
@@ -1,20 +1,49 @@
 // set global variables
-let dataEmployees = [];
-let dataRoles = [];
+let employees = [];
+let roles = [];
 let itemSelected;
 
-const clearButton = document.querySelector('#clear');
+// form
 const form = document.querySelector('form');
+// list
+const employeesList = document.querySelector('#list-employees');
+// buttons
+const clearButton = document.querySelector('#clear');
+const submitButton = document.querySelector('#submit');
+const deleteButton = document.querySelector('#delete');
+
+// when the page is loaded, the init function will run
+// set on int all the methos you also want to run once the page is loaded
+
+/* 
+fetching data: considering that we are reading two different data, and we want to grab their results at once
+the default behaviour for the requests is: after the first one is loaded, then, start the second
+to avoid this behaviour, we should use Promise.all(), that saves the requests into arrays and displays its results, once all data is loaded and ready to use
+*/
 
 async function init() {
-    // set the methods to be called once the page is load
-    await fetchData();
+    // methods
+    // destructuring Promise.all() arrays set above as global
+    [employees, roles] = await
+        Promise.all([
+            listEmployees(),
+            listRoles(),
+        ]);
     readEmployees();
     readRoles();
 
+    clearSelection();
+
     // event listeners
     clearButton.addEventListener('click', () => clearData(event));
+
+    // passing the event submit to form will consider the event from the button submit and also from the 'enter - pressed' events as submition
+    form.addEventListener('submit', () => submitData(event));
+    form.addEventListener('change', () => setButtons(event));
+
+    deleteButton.addEventListener('click', () => deleteItem(event));
 }
+
 init();
 
 function selectItem(employee, event) {
@@ -27,15 +56,22 @@ function selectItem(employee, event) {
     // then, add the class
     element.classList.add('selected');
     console.log(itemSelected);
+
     // once we have the element select from the list, we should display its values on the form 
     // lets use query selector form to access the form elements
     form.name.value = employee.name;
     form.salary.valueAsNumber = employee.salary;
     form.role_id.value = employee.role_id;
+
+    // change the text from submit button
+    submitButton.textContent = "Update";
+    clearButton.classList.remove('disabled');
+    deleteButton.classList.remove('disabled');
 }
 
 // removing class selected
 function clearSelection() {
+    itemSelected = undefined;
     // when called, this function will get by querySelector which li has the class selected on it
     let elementSelected = document.querySelector('li.selected');
     // then, through a statement will check:
@@ -47,8 +83,19 @@ function clearSelection() {
     // once is done, call this function at the beginning of the selectItem() method
     // the function will remove a selected class before add a new one
     // also, apply his function to the clearData(), to run every time the clear button is clicked
+
+    // change the text from submit button
+    submitButton.textContent = "Create";
+    clearButton.classList.add('disabled');
+    deleteButton.classList.add('disabled');
 }
 
+function setButtons (event)
+{
+    event.preventDefault();
+    clearButton.classList.remove('disabled');
+    deleteButton.classList.remove('disabled');
+}
 // clear all inputs and remove selected class
 function clearData(event) {
     event.preventDefault();
@@ -58,57 +105,106 @@ function clearData(event) {
     clearSelection();
 }
 
-// considering that we are reading two different data, we want to grab their results at once
-// the default behaviour for the requests is: once one is finished, then, start the second
-// to avoid this behaviour, we should use Promise.all(), that saves the requests into arrays and displays its results, once all data is ready to use
-async function fetchData() {
-    // add try/catch
-    try {
-        // destructuring the promises from Promise.all() into arrays amd make the variables as global
-        [dataEmployees, dataRoles] =
-            await Promise.all([
-                listEmployees(),
-                listRoles(),
-            ])
-    } catch (error) {
-        showError(error);
+// submiting form
+async function submitData(event) {
+    console.log("submiting data");
+    event.preventDefault();
+
+    // from here, whe should grab the value from form inputs and save it into an object
+    const employeeData = {
+        // id: itemSelected.id,
+        name: form.name.value,
+        salary: Number(form.salary.value),
+        role_id: Number(form.role_id.value)
+    };
+
+    // now, call the update function, passing as its arguments the info we have saved into the obj
+
+    // if there is any item selected, them consider the action as update
+    if (itemSelected) {
+        // updating the backend:
+        // call the request - pass it into a variable
+        const updatedItem = await updateEmployeeRegister(itemSelected.id, employeeData);
+
+        // updating the front end:
+        // pass the updated element index into the employees array 
+        const index = employees.indexOf(itemSelected);
+        employees[index] = updatedItem;
+        // in that way, we are going to update just the index que have update 
     }
-    console.log("fecthing data")
+    // otherwise, consider the action as a new register
+    else {
+        // adding to the the backend:
+        //  call the request
+        const createdItem = await createEmployeeRegister(employeeData);
+
+        // adding to the front end:
+        // add the created element to the array by using push()
+        employees.push(createdItem);
+        // employees.push(employeeData);
+    }
+    // now, lets render again the list to be displayed
+    readEmployees();
+    // error!! it is loading the app when call the backend!!
 }
 
-function readEmployees() {
-    // variables
-    const employeesList = document.querySelector('#list-employees');
+async function deleteItem(event) {
+    event.preventDefault();
+    if (itemSelected) {
+        // removing from the backend:
+        // call de request:
+        await deleteEmployeeRegister(itemSelected.id);
 
-    // first, read the employes data
-    dataEmployees.forEach((employee) => {
-        // second, find in roles the role id that matches (==) the employee role_id
-        // save this result into a variable
-        let listRole = dataRoles.find((role) => role.id == employee.role_id);
+        // removing from the front end:
+        // to remove a item from an array, lets use splice(i, 1)
+        const index = employees.indexOf(itemSelected);
+        employees.splice(index, 1);
+        readEmployees();
+        clearData(event);
+    }
+}
+
+// renderig emlpyees data
+function readEmployees() {
+    // setting the list to empty
+    employeesList.innerHTML = "";
+
+    // read the array 
+    employees.forEach((employee) => {
+
+        // pass the array roles to find on it the role_id that matches (==) the employee role_id
+        // save this result into a variable  and return      
+        let listRole = roles.find((role) => { return role.id == employee.role_id });
+
         // then, for each item, create and element
         const li = document.createElement('li');
+
         const spanName = document.createElement('span');
         spanName.textContent = employee.name + ",";
+
         const spanRole = document.createElement('span');
         spanRole.textContent = listRole.name + ",";
+
         const spanSalary = document.createElement('span');
         spanSalary.textContent = employee.salary;
+
         // append spans to li
         li.appendChild(spanName);
         li.appendChild(spanRole);
         li.appendChild(spanSalary)
+
         // finally, append li to ul
         employeesList.appendChild(li);
 
         // for each li, we will add a event listener, to identify which item was clicked
         li.addEventListener('click', () => selectItem(employee, event));
     })
-    console.log("read employess data")
 }
 
+// rendering roles date
 function readRoles() {
-    // read the employes data
-    dataRoles.forEach((role) => {
+    // read the the array
+    roles.forEach((role) => {
         // then, for each item, create and element
         const option = document.createElement('option');
         // set its context and values
@@ -117,7 +213,6 @@ function readRoles() {
         // and finally, append option to the select
         form.role_id.appendChild(option);
     })
-    console.log("read roles data")
 }
 
 function showError(error) {
--- module00-javascript/challenge02-crud-app/frontend/js/requests.js
+++ module00-javascript/challenge02-crud-app/frontend/js/requests.js
@@ -1,29 +1,60 @@
 // set here all http requests
 
 // variables
-const baseURL = "http://localhost:3000/";
+const baseURL = "http://localhost:3000";
 
 // create a function by passing the fetch api to access the backend
 
 // considering that a promisse is created just inside a fecth request, to get employee and role data to be displayed side by side, we should consider using a nested fetch request
 // trying to keep the code clean, we could pass the requests into modules
 
-// first, create a function that will return the response as json()
-function returnFetchJson(url) {
-    return fetch(url).then((response) => {
-        return response.json();
+// first, create a function that will return the response as json() and also any errors if there is one
+function returnFetchJson(url, options) {
+    return fetch(url, options).then((response) => {
+        // if response is ok, then return response.json
+        if (response.ok) {
+            return response.json();
+        } else {
+            // throw a error
+            throw new Error(response.statusText);
+        }
+    }).catch(error => {
+        throwError("Data loading error", error);
+        throw error;
     });
 }
 
 // this module returns the fetch for epmployess
 function listEmployees() {
-    return returnFetchJson(`${baseURL}employees`);
+    return returnFetchJson(`${baseURL}/employees`);
 }
 
 // this module returns the fetch for roles
 function listRoles() {
-    return returnFetchJson(`${baseURL}roles`);
+    return returnFetchJson(`${baseURL}/roles`);
+}
+// now, pass these functions over the request
+
+// set here the requests
+function createEmployeeRegister(employee) {
+    return returnFetchJson(`${baseURL}/employees`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(employee),
+    });
 }
 
-// now, pass this function over the request
+function updateEmployeeRegister(id, employee) {
+    return returnFetchJson(`${baseURL}/employees/${id}`, {
+        method: 'PUT',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(employee),
+    });
+}
+
+function deleteEmployeeRegister(id) {
+    return returnFetchJson(`${baseURL}/employees/${id}`, {
+        method: 'DELETE',
+    });
+}
 
